4c3087ecb89c2028e502a72fcb7290e7c0562161
fsauser <fsa@gmail.com>
2024-12-29 15:07:51 +0000

    Added code  duplication

--- src/main/java/test/app/App.java
+++ src/main/java/test/app/App.java
@@ -13,4 +13,12 @@ public class App
             System.out.println( "Hello  World" );
 
     }
+   public int task1(int a, int b) {
+    return a + b;
+    }
+
+   public int task2(int a, int b) {
+    int sum = a  + b;
+    return sum;
+  }
 }
